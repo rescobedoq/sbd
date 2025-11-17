e09a65907e78adf1155c92fd23e0c22b4d47f9b9
Raul-Almanza <ealmanzam@unsa.edu.pe>
2025-11-17 13:56:38 -0500

    implementado crud de usuarios

--- Biblioteca/Controllers/UsuarioController.h
+++ Biblioteca/Controllers/UsuarioController.h
@@ -17,12 +17,20 @@ class UsuarioController
 public:
     UsuarioController();
 
+    // Metodos del CRUD
+    bool actualizarUsuario(const int& id,const QString& nombre);
+    bool agregarUsuario(const int& id,const QString& nombre);
+    bool eliminarUsuario(int id);
+
+    // Metodos utilitarios
     bool cargarUsuarios();
     bool guardarUsuarios(const QString& archivo);
-
-    bool agregarUsuario(const int& id,const QString& nombre);
-    void eliminarUsuario(int indice);
     QVector<std::shared_ptr<Usuario>>& obtenerUsuarios();
+    std::shared_ptr<Usuario> obtenerUsuario(const int& pos);
+    int obtenerIndicePorID(int id);
+
+    // Metodos para busqueda
+    std::shared_ptr<Usuario> obtenerUsuarioPorID(int id);
 
 private:
     UsuarioDAO dao;
